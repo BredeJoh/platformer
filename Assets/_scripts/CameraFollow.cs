e475ae96d2da42ddfd3d1b3f62b1cac9f19cbaad
BredeJoh <brede_96@hotmail.com>
2016-09-27 20:47:54 +0200

    start

---
+++ Assets/_scripts/CameraFollow.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform player;
+    Vector3 offset;
+    public float rotationSpeed = 30f;
+    Vector3 distance;
+
+	// Use this for initialization
+	void Start () {
+	    offset = new Vector3(0f, 5f, -9f);
+        gameObject.transform.position = player.position + offset; 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        distance = player.position - gameObject.transform.position;
+        print(distance.magnitude);
+        Vector3 input = Vector3.zero;
+        input.x = Input.GetAxis("rightHorizontal");
+        input.y = Input.GetAxis("rightVertical");
+        
+
+        gameObject.transform.LookAt(player, Vector3.up);
+        
+        if (input.x >= 0.7)
+        {
+            gameObject.transform.RotateAround(player.position, Vector3.down, rotationSpeed * 2f * Time.deltaTime);
+            //gameObject.transform.Translate(Vector3.right * Time.deltaTime*rotationSpeed);
+        }
+        else if(input.x <= -0.7)
+        {
+            //gameObject.transform.Translate(Vector3.left * Time.deltaTime*rotationSpeed);
+            gameObject.transform.RotateAround(player.position, Vector3.up, rotationSpeed* 2f * Time.deltaTime);
+        }
+        if (input.y >= 0.7)
+        {
+            //gameObject.transform.RotateAround(player.position, Vector3.right, rotationSpeed * Time.deltaTime);
+            //gameObject.transform.Translate(Vector3.up * Time.deltaTime * rotationSpeed / 2f);
+        }
+        else if (input.y <= -0.7)
+        {
+            //gameObject.transform.RotateAround(player.position, Vector3.left, rotationSpeed * Time.deltaTime);
+            //gameObject.transform.Translate(Vector3.down * Time.deltaTime * rotationSpeed / 2f);
+        }
+        
+    }
+}
---
+++ Assets/_scripts/character_Movement.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character_Movement : MonoBehaviour {
+
+    public CharacterController myController;
+    public float groundSpeed = 6f;
+    public float AerialSpeed = 3f;
+    public float GravityStrenght = 9.8f;
+    public float jumpSpeed = 10f;
+    public Transform CameraTransform;
+    private bool canJump = false;
+    float verticalVelocity;
+    Vector3 velocity;
+    Vector3 groundedVelocity;
+    Vector3 normal;
+    bool onWall = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        //gameObject.transform.rotation = camera.transform.rotation;
+        GravityStrenght = 9.8f;
+        if (onWall && verticalVelocity < 0f)
+            GravityStrenght = 4.9f;
+        
+        Vector3 myVector = Vector3.zero;
+        Vector3 input = Vector3.zero;
+        input.x = Input.GetAxis("Horizontal");
+        input.z = Input.GetAxis("Vertical");
+        input = Vector3.ClampMagnitude(input, 1f);
+        Quaternion inputRotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(CameraTransform.forward, Vector3.up), Vector3.up);
+        if (myController.isGrounded)
+        {
+            myVector = input;
+            
+            myVector = inputRotation * myVector;
+            myVector *= groundSpeed;
+        }
+        else
+        {
+            myVector = groundedVelocity;
+            myVector += inputRotation * input * AerialSpeed;
+        }
+        myVector = Vector3.ClampMagnitude(myVector, groundSpeed);
+        myVector = myVector * Time.deltaTime;
+
+        verticalVelocity = verticalVelocity - GravityStrenght * Time.deltaTime;
+        
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (onWall)
+            {
+                Vector3 reflection = Vector3.Reflect(velocity, normal);
+                Vector3 projected = Vector3.ProjectOnPlane(reflection, Vector3.up);
+                groundedVelocity = projected.normalized * groundSpeed + normal * AerialSpeed;
+            }    
+            if (canJump)
+                verticalVelocity += jumpSpeed;
+        }
+        myVector.y = verticalVelocity * Time.deltaTime;
+
+        CollisionFlags flags = myController.Move(myVector);
+        // player speed
+        velocity = myVector / Time.deltaTime;
+
+        if ((flags & CollisionFlags.Below) != 0)
+        {
+            groundedVelocity = Vector3.ProjectOnPlane(velocity, Vector3.up);
+            canJump = true;
+            onWall = false;
+            verticalVelocity = -3f;
+        }
+        else if ((flags & CollisionFlags.Sides) != 0f)
+        {
+            canJump = true;
+            onWall = true;
+            
+        }
+        else if ((flags & CollisionFlags.Above) != 0f)
+        {
+            verticalVelocity = -2f;
+            canJump = false;
+            onWall = false;
+        }
+        else
+        {
+            canJump = false;
+            onWall = false;
+        }
+        if (verticalVelocity > 8f)
+            verticalVelocity = 8f;
+        //print(verticalVelocity);
+	}
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+       normal = hit.normal;   
+    }
+}
